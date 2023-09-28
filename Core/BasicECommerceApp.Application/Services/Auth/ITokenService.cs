9c535318d5f9e66193010432c86d6fd7dff6ac7d
Serkan IŞIK <dehnas79@gmail.com>
2023-09-28 12:51:06 +0300

    Add ITokenService

---
+++ Core/BasicECommerceApp.Application/Services/Auth/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using BasicECommerceApp.Application.Configurations.Auth;
+using BasicECommerceApp.Application.DTOs.Auth;
+using BasicECommerceApp.Domain.Entities.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicECommerceApp.Application.Services.Auth
+{
+    public interface ITokenService
+    {
+        Task<TokenDto> CreateTokenAsync(AppUser userApp);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
