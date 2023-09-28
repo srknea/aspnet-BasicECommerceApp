06f95e9b3d9be707c0d285c8ad38f29e68c4172b
Serkan IŞIK <dehnas79@gmail.com>
2023-09-28 13:37:51 +0300

    Configurations for entities

---
+++ Infrastructure/BasicECommerceApp.Persistance/Configurations/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using BasicECommerceApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicECommerceApp.Persistance.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Stock).IsRequired();
+        }
+    }
+}
--- Infrastructure/BasicECommerceApp.Persistance/Contexts/BasicECommerceAppDbContext.cs
+++ Infrastructure/BasicECommerceApp.Persistance/Contexts/BasicECommerceAppDbContext.cs
@@ -2,6 +2,7 @@
 using BasicECommerceApp.Domain.Entities.Auth;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,10 +19,12 @@ namespace BasicECommerceApp.Persistance.Contexts
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+            // Configurations for entities
+            builder.ApplyConfigurationsFromAssembly(GetType().Assembly); // IEntityTypeConfiguration 'ı implement eden class'ları bulduk…
 
             base.OnModelCreating(builder);
         }
