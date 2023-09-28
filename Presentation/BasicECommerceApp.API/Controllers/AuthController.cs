70b6fd181b6df9080f036dac2f194afe4f024b8f
Serkan IŞIK <dehnas79@gmail.com>
2023-09-28 19:56:34 +0300

    Add AuthController

---
+++ Presentation/BasicECommerceApp.API/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using BasicECommerceApp.Application.DTOs.Auth;
+using BasicECommerceApp.Application.Services.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicECommerceApp.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : CustomBaseController
+    {
+        private readonly IAuthenticationService _customAuthenticationService;
+
+        public AuthController(IAuthenticationService customAuthenticationService)
+        {
+            _customAuthenticationService = customAuthenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            var result = await _customAuthenticationService.CreateTokenAsync(loginDto);
+
+            /*
+            if (result.StatusCode == 200)
+            {
+                return Ok();
+            }
+            else if(result.StatusCode == 404)
+            {
+                return NotFound();
+            }
+            */
+
+            return CreateActionResult(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            var result = _customAuthenticationService.CreateTokenByClient(clientLoginDto);
+
+            return CreateActionResult(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _customAuthenticationService.RevokeRefreshToken(refreshTokenDto.RefreshToken);
+
+            return CreateActionResult(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
+
+        {
+            var result = await _customAuthenticationService.CreateTokenByRefreshToken(refreshTokenDto.RefreshToken);
+
+            return CreateActionResult(result);
+        }
+    }
+}
