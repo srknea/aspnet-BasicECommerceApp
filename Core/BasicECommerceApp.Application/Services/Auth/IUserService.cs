2664fcd600de317d0cc30942d6784484cadbf847
Serkan IŞIK <dehnas79@gmail.com>
2023-09-28 13:19:28 +0300

    Add IUserService

---
+++ Core/BasicECommerceApp.Application/Services/Auth/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using BasicECommerceApp.Application.DTOs.Auth;
+using BasicECommerceApp.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicECommerceApp.Application.Services.Auth
+{
+    public interface IUserService
+    {
+        Task<CustomResponseDto<AppUserDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<CustomResponseDto<AppUserDto>> GetUserByNameAsync(string userName);
+    }
+}
