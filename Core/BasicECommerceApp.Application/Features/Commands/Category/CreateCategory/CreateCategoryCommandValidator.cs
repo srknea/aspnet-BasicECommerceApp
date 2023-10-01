6fe91b53c045ac865e29d407745b42d6c0194076
Serkan IŞIK <dehnas79@gmail.com>
2023-10-01 16:29:40 +0300

    Add validation for CreateCategory and CreateSubCategory

---
+++ Core/BasicECommerceApp.Application/Features/Commands/Category/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicECommerceApp.Application.Features.Commands.Product.CreateProduct
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommandRequest>
+    {
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
+        }
+    }
+}
